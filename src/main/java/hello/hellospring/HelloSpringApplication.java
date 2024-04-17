3e9df1712c1b0cabe9ebcff65154de4b8e6db500
백원재 <81965519+Cinema-Paradiso@users.noreply.github.com>
2024-04-17 14:38:00 +0900

    first

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,19 @@
+package hello.hellospring;
+
+import hello.hellospring.domain.Member;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+		Member member = new Member();
+		System.out.println("member = " + member);
+		System.out.println(member.getClass());
+		System.out.println("sefnl");
+
+
+	}
+}
